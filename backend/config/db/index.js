d3bb7041a1cb5c088678ed9812b6d825d2419208
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-01-08 19:44:03 +0700

    fix

--- backend/config/db/index.js
+++ backend/config/db/index.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Successfully');
-  } catch (error) {
-    console.log('Fail');
+class MongoConnection {
+  constructor(conString) {
+    this.conString = conString;
   }
-};
 
-module.exports = connectDB;
+  static getInstance(conString) {
+    if (!this.instance) {
+      this.instance = new MongoConnection(conString);
+    }
+
+    return this.instance;
+  }
+
+  async connect() {
+    try {
+      await mongoose.connect(this.conString);
+      console.log('Successfully');
+    } catch (error) {
+      console.log('Fail');
+    }
+  }
+}
+
+const MongoConnectionInstance = MongoConnection.getInstance(process.env.MONGO_URI);
+
+module.exports = MongoConnectionInstance;
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const route = require('./routes');
-const connectDB = require('./config/db');
+const MongoConnectionInstance = require('./config/db');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-connectDB();
+MongoConnectionInstance.connect();
 
 route(app);
 app.use(express.static('frontend'));
